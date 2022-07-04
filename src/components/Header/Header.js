04876d5163c9d966f0c52228d24a11f6f8c46e8a
emre-cil <info.emrecil@gmail.com>
2022-07-04 23:45:12 +0300

    header updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,16 +6,11 @@ import { flexBetweenCenter, displayOnDesktop } from 'themes/commonStyles';
 import LocationSearch from './Search/LocationSearch';
 import ProfileSettings from './ProfileSettings';
 import MobileSearch from './Search/MobileSearch';
+import styled from 'styled-components';
 
 const Header = () => {
   return (
-    <Box
-      sx={{
-        ...flexBetweenCenter,
-        minHeight: 90,
-        borderBottom: '1px solid #ddd',
-      }}
-    >
+    <Wrapper>
       <Box sx={displayOnDesktop}>
         <Logo />
       </Box>
@@ -28,8 +23,17 @@ const Header = () => {
       <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
         <MobileSearch />
       </Box>
-    </Box>
+    </Wrapper>
   );
 };
 
+const Wrapper = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media screen and (min-width: 744px) {
+    border-bottom: 1px solid var(--medGrey);
+    padding: 14px 0 18px;
+  }
+`;
 export default Header;
